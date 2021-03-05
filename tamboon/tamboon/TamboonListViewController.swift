a831c5ba6a3bd426cd349a4e5030b6b283a3f71d
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2021-03-05 12:52:05 +0700

    Add tamboon tableview

--- tamboon/tamboon/TamboonListViewController.swift
+++ tamboon/tamboon/TamboonListViewController.swift
@@ -9,12 +9,28 @@ import UIKit
 
 final class TamboonListViewController: UIViewController {
     
+    private var tableView = UITableView()
+    
     private var viewModel: TamboonListViewControllerViewModelInterface!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.title = "Tamboon"
+        
         viewModel = TamboonListViewControllerViewModel(delegate: self)
+        
+        // Setup tableview
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        let safeArea = view.safeAreaLayoutGuide
+        tableView.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor).isActive = true
     }
 }
 
@@ -22,4 +38,17 @@ final class TamboonListViewController: UIViewController {
 
 extension TamboonListViewController: TamboonListViewControllerViewModelDelegate {
     
+}
+
+// MARK: - UITableViewDataSource, UITableViewDelegate
+
+extension TamboonListViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
 }
